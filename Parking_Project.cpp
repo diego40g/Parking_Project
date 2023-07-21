3b4c19cbfb6d4969614df0de5fc41d0941803ad8
Diego Ignacio Paz Naula <diego.paz@institutotraversari.edu.ec>
2023-07-21 17:53:54 -0500

    updating the main who only use a library with required programas

--- Parking_Project.cpp
+++ Parking_Project.cpp
@@ -1,9 +1,20 @@
-#include "RequiredLibraries.h";
+#include "RequiredPrograms.h";
+
+#define _CRT_SECURE_NO_WARNINGS
 
 int main()
 {
-    cout << "Mi parquedero" << endl;
+	cout<<menu();
 
+	
+	carrito();
+
+	char QR[50] = "";
+	strcpy_s(QR, "Nombre estudiante\n");
+	strcat_s(QR, "Estudiante\n");
+	strcat_s(QR, "ISTPET\n");
+	strcat_s(QR, "27/07/2023\n");
+	generarQr(QR);
     return 0;
 }
 
